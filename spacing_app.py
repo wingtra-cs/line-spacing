a0eb02865f3e189d38dc455b4187b78fbf37ed09
wingtra-cs <84965828+wingtra-cs@users.noreply.github.com>
2023-06-22 12:07:16 +0800

    Update spacing_app.py
    Added error handling

--- spacing_app.py
+++ spacing_app.py
@@ -8,7 +8,7 @@ st.title('Flight Line Spacing Calculator')
 st.sidebar.image('./logo.png', width = 260)
 st.sidebar.markdown('#')
 st.sidebar.write('This application computes the flight line spacing for a given Wingtra payload.')
-st.sidebar.write('Please note that these are approximate and ideal. Some variations in-field are expected.')
+st.sidebar.write('Please note that these are approximations under ideal conditions. Some variations during actual flight are expected.')
 st.sidebar.write('If you have any questions regarding the application, please contact us at support@wingtra.com.')
 st.sidebar.markdown('#')
 st.sidebar.info('This is a prototype application. Wingtra AG does not guarantee correct functionality. Use with discretion.')
@@ -20,7 +20,12 @@ camera = {'RGB61': {'f': 24, 'w': 35.7, 'h': 23.9},
           'Micasense RedEdge-P': {'f': 10.3, 'w': 8.5, 'h': 7.1}}
 
 choice = st.selectbox('📷 Select Payload Used :',
-    ('[Payload]', 'RGB61', 'RX1', 'a6100 Nadir', 'a6100 Oblique', 'Micasense RedEdge-P'))
+                      ('[Payload]', 
+                       'RGB61', 
+                       'RX1', 
+                       'a6100 Nadir', 
+                       'a6100 Oblique', 
+                       'Micasense RedEdge-P'))
 
 h = st.number_input('🛫 Height Above Ground (meters)')
 ovr = st.number_input('🥃 Side Overlap (%)')/100
@@ -35,7 +40,10 @@ else:
     fov = 2*math.atan(w/(2*f))
     d = 2*h*math.tan(fov/2)
     
-    s = ((1-ovr)*d)
+    s = (1-ovr)*d
 
-if st.button('Compute Flight Line Spacing'):
-    st.text(f'Line spacing for the mission is {round(s,2):.2f} meters.')
+if h == 0 or ovr == 0:
+    st.stop()
+else:
+    if st.button('Compute Flight Line Spacing'):
+        st.text(f'Line spacing for the mission is {round(s,2):.2f} meters.')
